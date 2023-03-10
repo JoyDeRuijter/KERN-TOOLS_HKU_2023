86b9763104ebcb390979770dae5ca36052bb0a89
Joy <joyderuijter@gmail.com>
2023-03-10 17:56:16 +0100

    Finish the visualization of the start menu

--- Assets/Scripts/StartMenu.cs
+++ Assets/Scripts/StartMenu.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class StartMenu : MonoBehaviour
 {
     [SerializeField] private Animator logoAnim;
+    [SerializeField] private Button[] buttons;
+    [SerializeField] private GameObject[] imageObjects;
 
     private void Start()
     {
@@ -15,5 +18,20 @@ public class StartMenu : MonoBehaviour
     {
         yield return new WaitForSeconds(3f);
         logoAnim.SetBool("HasStarted", true);
+        yield return new WaitUntil(() => logoAnim.GetCurrentAnimatorStateInfo(0).IsName("End") == true);
+        yield return new WaitForSeconds(1f);
+        StartCoroutine(ButtonsAppear());
+    }
+
+    private IEnumerator ButtonsAppear()
+    {
+        for (int i = 0; i < buttons.Length; i++)
+        {
+            buttons[i].GetComponent<Button>().interactable = true;
+            yield return new WaitForSeconds(1f);
+        }
+        imageObjects[0].gameObject.SetActive(true);
+        yield return new WaitForSeconds(1.1f);
+        imageObjects[1].gameObject.SetActive(true);
     }
 }
